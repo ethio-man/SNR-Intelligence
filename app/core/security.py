9b5ec189608acd81d6a154014a3c6a07a32cabe9
ethio-man <natymiskir@gmail.com>
2026-04-22 10:26:12 -0300

    password hashing implemented

--- app/core/security.py
+++ app/core/security.py
@@ -1,5 +1,9 @@
 from passlib.context import CryptContext
+from datetime import datetime,timedelta
+from jose import jwt
+import os
 
+ACCESS_TOKEN_EXPIRE_MINUTES = 60
 pwd_context=CryptContext(schemes=['bcrypt'],deprceated='auto')
 
 def hash_password(password:str):
@@ -7,3 +11,8 @@ def hash_password(password:str):
 
 def verify_password(plain_password:str, hashed_password:str):
     return pwd_context.verify(plain_password,hashed_password)
+
+def create_access_token(data:dict,expires_delta:timedelta):
+    to_encode=data.copy()
+    expire=datetime.utcnow() +( expires_delta or timedelta.)
+    
