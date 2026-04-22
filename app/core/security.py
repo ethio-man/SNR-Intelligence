e16cb54caa7cce6be93264f9570f58b71a70da13
ethio-man <natymiskir@gmail.com>
2026-04-22 10:34:30 -0300

    create token implemented

--- app/core/security.py
+++ app/core/security.py
@@ -12,7 +12,8 @@ def hash_password(password:str):
 def verify_password(plain_password:str, hashed_password:str):
     return pwd_context.verify(plain_password,hashed_password)
 
-def create_access_token(data:dict,expires_delta:timedelta):
+def create_access_token(data:dict,expires_delta:timedelta |None=None):
     to_encode=data.copy()
-    expire=datetime.utcnow() +( expires_delta or timedelta.)
-    
+    expire=datetime.utcnow() +( expires_delta or timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
+    to_encode.update({'exp':expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
